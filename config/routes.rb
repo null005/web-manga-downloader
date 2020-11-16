5a2d47830bd96ffd1a4e995f709897877dd0a1ab
yyyyy <ipooo346@gmail.com>
2020-11-16 14:18:56 -0300

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/welcome'
+
   resources :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#index'
+  root to: 'sessions#index'
 end
