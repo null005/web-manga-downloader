b959aea65d583f1a599335504013ec52010904fa
yyyyy <ipooo346@gmail.com>
2020-11-23 14:32:26 -0300

    added logout post route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'authorized', to: 'sessions#page_requires_login'
 
-  get 'logout', to: 'sessions#logout'
+  post 'logout', to: 'sessions#logout'
 
   root to: 'sessions#welcome'
 end
