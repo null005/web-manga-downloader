c3fb48afa8a78aa2016b57c35340fea5a27ddd60
yyyyy <ipooo346@gmail.com>
2021-01-18 15:45:08 -0300

    set route pos

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
 
-  post 'login', to: 'sessions#create'
-
   get 'welcome', to: 'sessions#welcome'
 
   get 'authorized', to: 'sessions#page_requires_login'
 
   get 'logout', to: 'sessions#logout'
+
+  post 'login', to: 'sessions#create'
 
   root to: 'sessions#welcome'
 end
